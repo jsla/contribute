5e25fdc9ac5b016dd9dd074f653fc369673a3e42
rouzbeh84 <rouzbeh.sarrafieh@gmail.com>
2018-05-31 01:08:48 -0700

    fixes double quotes

--- client/views/components/sponsor-preview.js
+++ client/views/components/sponsor-preview.js
@@ -2,11 +2,11 @@ const html = require('choo/html')
 
 module.exports = opts => {
   return html`
-    <div class="mt3">
-      <label class="db fw4 lh-copy f6">
-        ${opts.label || "Preview Image"}
+    <div class='mt3'>
+      <label class='db fw4 lh-copy f6'>
+        ${opts.label || 'Preview Image'}
       </label>
-      <div style="${`
+      <div style='${`
         box-shadow: 0 0 2 rgba(0,0,0,.2);
         background-color: white;
         background-image: url(${opts.url});
@@ -14,7 +14,7 @@ module.exports = opts => {
         background-position: center center;
         width: 275px;
         height: 100px;
-      `}"></div>
+      `}'></div>
     </div>
   `
 }
