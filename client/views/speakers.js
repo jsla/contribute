8b73930a6de145a39362bdc84ff064959d6c324e
rouzbeh84 <rouzbeh.sarrafieh@gmail.com>
2018-05-31 00:51:26 -0700

    feat: add `updatedAt` to speakers

--- client/views/speakers.js
+++ client/views/speakers.js
@@ -153,6 +153,7 @@ module.exports = function (state, prev, send) {
 
   function update (key, value) {
     send('updateSpeaker', {key: key, value: value})
+    send('updateSpeaker', {key: 'updatedAt', value: new Date().toUTCString()})
   }
 }
 
